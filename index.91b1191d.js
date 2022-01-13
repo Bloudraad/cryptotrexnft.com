eca39fafe6199b2fc59f0cd679a813a066b20e7d
owenyuwono <owenyuwono@users.noreply.github.com>
2022-01-13 12:43:59 +0000

    deploy: 135b2e9c8fd20924de13f86e5bb7512ff4504e78

---
+++ index.91b1191d.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelector("#preview .voxel-viewer");e.style.height=e.offsetWidth,window.addEventListener("resize",(()=>{e.style.height=e.offsetWidth}))}));
+//# sourceMappingURL=index.91b1191d.js.map
--- migrate.eda7b5e5.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".question h3").forEach((e=>{e.addEventListener("click",(()=>{e.classList.toggle("active")}))}));
-//# sourceMappingURL=migrate.eda7b5e5.js.map
