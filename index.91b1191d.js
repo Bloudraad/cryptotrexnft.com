c3befeac9ec6c8e66ccd6aad08b483002d830cc3
owenyuwono <owenyuwono@users.noreply.github.com>
2022-01-16 22:59:18 +0000

    deploy: 2b07379425fe024de6c47a1606843c40e0d2f3e7

--- index.91b1191d.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelector("#preview .voxel-viewer");e.style.height=e.offsetWidth,window.addEventListener("resize",(()=>{e.style.height=e.offsetWidth}))}));
-//# sourceMappingURL=index.91b1191d.js.map
