a712790ad90e027b8396709b4d51190ff3419284
owenyuwono <michael.oswin@ovo.id>
2022-01-17 07:25:50 +0700

    fix: rearrang total supply

--- js/main.js
+++ js/main.js
@@ -281,10 +281,6 @@ let vxaddress;
 window.onload = async () => {
   const txtMinted = document.getElementById('txtMinted');
   const web3 = await loadWeb3();
-  const address = await web3Address(web3);
-  const chainId = await web3.eth.getChainId();
-  const vxc = new web3.eth.Contract(vx.abi, config[chainId].vx_address);
-  vxaddress = config[chainId].vx_address;
   const supply = await vxc.methods.totalSupply().call({});
   if (supply > 13333) {
     txtMinted.textContent = `Sold out!`;
@@ -292,6 +288,10 @@ window.onload = async () => {
     btnFossilToggle.hidden = true;
   }
   txtMinted.textContent = `${supply} / 13,333 Minted`;
+  const address = await web3Address(web3);
+  const chainId = await web3.eth.getChainId();
+  const vxc = new web3.eth.Contract(vx.abi, config[chainId].vx_address);
+  vxaddress = config[chainId].vx_address;
   const ctc = new web3.eth.Contract(ct.abi, config[chainId].migration_address);
   const balance = await ctc.methods.balanceOf(address).call({});
   btnFossilToggle.hidden = balance == 0;
