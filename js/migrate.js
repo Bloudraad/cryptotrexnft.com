9209e79a7ca17df7e2290cb55d6a089a60ff307e
Bloudraad <101117021+Bloudraad@users.noreply.github.com>
2024-04-27 17:12:15 +0200

    Update migrate.js

--- js/migrate.js
+++ js/migrate.js
@@ -243,15 +243,19 @@ if (v2) {
         console.log('Constructed URL:', apiUrl);
 
         // Make the fetch request
-        const response = await fetch(
-          apiUrl,
-          {
-            method: 'GET',
-            headers: {
-              'X-API-KEY': config[chainId].opensea_api_key,
-            },
-          }
-        );
+   const response = await fetch(
+  apiUrl,
+  {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': config[chainId].opensea_api_key,
+    },
+  }
+);
+
+// Log whether the API key is included in the request headers
+console.log('API Key Used:', response.headers.has('X-API-KEY'));
+
 
         // Handle the response...
       } catch (error) {
