2eeb2e02bbddd48d7e0a9805eea9653a4ca43791
Bloudraad <101117021+Bloudraad@users.noreply.github.com>
2024-05-01 13:02:06 +0200

    Update migrate.js

--- js/migrate.js
+++ js/migrate.js
@@ -252,30 +252,11 @@ v2.forEach(async (e) => {
   console.log("Constructed URL_v2:", url);
   console.log("Headers:", options.headers); // Logging headers to check if the API key is included
 
-  try {
-    const response = await fetch(url, options); // Define response within the loop
-    console.log("Response:", response); // Log the response object
-    const body = await response.json(); // Move this line inside the try block
-    console.log("Body:", body); // Log the response body
-
-    // Check if body contains the expected structure
-    if (body && body.nft && body.nft.is_suspicious) {
-      console.log("NFT is suspicious:", body.nft.is_suspicious);
-
-      // Here, just before calling buildCard, add your console.log statement
-      console.log("Before calling buildCard for item:", body);
-
-      // Call buildCard function
-      const cardContainer = buildCard(body, migrated);
-
-      // Append cardContainer to wherever you want in your DOM
-    } else {
-      console.error("Invalid or missing data in the response:", body);
-    }
-  } catch (error) {
+ catch (error) {
     console.error(error);
+      }
+    });
   }
-});
   
 function buildCard(e, migrated) {
   // Check if the image URL is nested under e.nft
