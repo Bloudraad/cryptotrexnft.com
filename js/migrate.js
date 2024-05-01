4a704afd45eabe794194f6bdff1c81eda5dcc4ee
Bloudraad <101117021+Bloudraad@users.noreply.github.com>
2024-05-01 12:41:49 +0200

    Update migrate.js

--- js/migrate.js
+++ js/migrate.js
@@ -251,38 +251,25 @@ v2.forEach(async (e) => {
   const url = `${config[chainId].opensea_api}/api/v2/chain/ethereum/contract/${config[chainId].migration_address}/nfts/${Web3.utils.toBN(e)}`; //nfts/
   console.log("Constructed URL_v2:", url);
   console.log("Headers:", options.headers); // Logging headers to check if the API key is included
-
   try {
     const response = await fetch(url, options); // Define response within the loop
     console.log("Response:", response); // Log the response object
     const body = await response.json(); // Move this line inside the try block
     console.log("Body:", body); // Log the response body
 
-    // Check if body contains the expected structure
-    if (body && body.nft && body.nft.is_suspicious) {
-      console.log("NFT is suspicious:", body.nft.is_suspicious);
-
-      // Here, just before calling buildCard, add your console.log statement
+     // Here, just before calling buildCard, add your console.log statement
       console.log("Before calling buildCard for item:", body);
-
-      // Call buildCard function
-      const cardContainer = buildCard(body, migrated);
-
       // Append cardContainer to wherever you want in your DOM
-    } else {
-      console.error("Invalid or missing data in the response:", body);
-    }
-  } catch (error) {
+    } 
+   catch (error) {
     console.error(error);
   }
 });
-
+}
   
 function buildCard(e, migrated) {
-  
   // Check if the image URL is nested under e.nft
   console.log("Image URL:", e.nft ? e.nft.image_url : e.image_url);
-  
   console.log("Image URL:", e.image.src);
   const card = document.createElement('div');
   card.classList = 'card';
